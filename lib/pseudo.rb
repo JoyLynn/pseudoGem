876b781ef7b7108b9d8f871b62f9bb02edd35529
JoyLynn <joylynnious@gmail.com>
2015-08-18 19:11:55 +0300

    added code to my ruby file..

--- lib/pseudo.rb
+++ lib/pseudo.rb
@@ -2,5 +2,18 @@ require "pseudo/version"
 
 module Pseudo
   # Your code goes here...
-  
+  def reverse(String)
+
+	reverse_string = ""
+
+	i = string.length-1
+
+	while i >= 0
+		reverse_string = original_string[i] + reverse_string
+		i -= 1	
+	end
+
+	return reverse_string
+
+  end
 end
